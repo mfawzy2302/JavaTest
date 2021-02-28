867e84dff9b2019070a154dfccc1b685b91bf074
mohramzy2021 <79584957+mohramzy2021@users.noreply.github.com>
2021-02-28 23:04:17 +0200

    Initial commit (3la bark allah)

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person p = new Person("Fawzy",23);
+        System.out.println(p.toString());
+        Person p2 =new Person("Ramzy",25);
+        System.out.println(p2.toString());
+    }
+}
